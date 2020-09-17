30e4897fdc64b6bebecaafc5d84ab94b2fcc975f
aurelien briard <aurelienbriard@gmail.com>
2020-09-17 17:10:20 +0200

    Ajout du CRUD sur la table plateforme
    Ajout des fichiers plateforme.class.php + plateformeManager.class.php
    Test des méthodes dans index
    Modif d'une erreur dans connexion.php

--- index.php
+++ index.php
@@ -5,13 +5,34 @@
 	<meta charset="utf-8">
 </head>
 <body>
-<?php 
+<?php
 
 	spl_autoload_register(function($classe) {
 		require 'classes/' .$classe. '.class.php';
 	});
 
 	require_once'inc/connexion.php';
+
+  //code de test
+  $plateformeManager = new PlateformeManager($bdd);
+
+  // nouvel objet plateforme
+  $plateforme = new Plateforme([
+    'id_plateforme' => 10,
+    'nom_plateforme' => 'premiere plateforme'
+  ]);
+
+  echo '<pre/>'; var_dump($plateforme);
+
+  // 1 - test de la méthode addPlateforme
+  //if( $plateformeManager->addPlateforme($plateforme) ) {echo 'plateforme ajoutée';} else { echo 'plateforme pas ajoutée';}
+
+  // 2 - test de la méthode updatePlateforme
+  //if( $plateformeManager -> updatePlateforme($plateforme) ) {echo 'plateforme modifiée';} else { echo 'plateforme pas modifiée';}
+
+  // 2 - test de la méthode deletePlateforme
+  if( $plateformeManager -> deletePlateforme($plateforme) ) {echo 'plateforme effacée';} else { echo 'plateforme pas effacée';}
+
 	?>
 
 </body>
