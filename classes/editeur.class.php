fce99ee7938bb0800217bd0e174f573875db9908
aurelien briard <aurelienbriard@gmail.com>
2020-09-18 09:35:26 +0200

    MAJ editeur

---
+++ classes/editeur.class.php
@@ -0,0 +1,40 @@
+<?php
+
+// Entité Editeur, la table Editeur avec ses propriétés
+class Editeur {
+
+  // je déclare des propriétés
+  private $id_editeur;
+  private $nom_editeur;
+  private $lien_editeur;
+
+  // constructeur de la classe avec un tableau de données en argument
+  public function __construct( array $donnees){
+    foreach ($donnees as $key => $value) { // je boucle sur le tableau
+      $methode = 'set'.ucfirst($key); // j'appel le setter dans une fonction
+      if (method_exists($this, $methode)){ // si le setter existe
+        $this -> $methode($value); // on appel le setter
+      }
+    }
+  }
+
+  // ********************************** les getters ***************************
+  public function getIdEditeur() { return $this->id_editeur; }
+  public function getNomEditeur() { return $this->nom_editeur; }
+  public function getLienEditeur() { return $this->lien_editeur; }
+  // ***************************************************************************
+
+  // ****************************** Les setters ***********************************
+  public function setId_Editeur($id_editeur) {
+    $this->id_editeur = $id_editeur;
+    }
+
+    public function setNom_Editeur($nom_editeur) {
+        $this->nom_editeur = $nom_editeur;
+    }
+
+    public function setLien_Editeur($lien_editeur) {
+        $this->lien_editeur = $lien_editeur;
+    }
+}
+
---
+++ classes/plateformeManager.class.php
@@ -0,0 +1,45 @@
+<?php
+class PlateformeManager{
+
+  // connecteur à la BDD en propriété de l'objet
+  private $pdo;
+
+  // son constructeur avec en paramètre le connecteur à la BDD
+  public function __construct($pdo){
+    $this->pdo = $pdo;
+  }
+
+  // méthode pour ajouter une plateforme
+  public function addPlateforme(Plateforme $plateforme){ // avec en paramètre un objet plateforme
+    $add_plateforme = $this -> pdo -> prepare( 'INSERT INTO plateforme (nom_plateforme) VALUES (:nom_plateforme)' ); // je^prépare la requete
+    $add_plateforme -> bindValue( ':nom_plateforme', $plateforme->getNomPlateforme(), PDO::PARAM_INT ); // j'affecte la valeur du formulaire au champ de la table
+    $add_plateforme -> execute();
+    $add_plateforme -> closeCursor();
+    return ($add_plateforme-> rowCount() ); // retourne le nbre de lignes
+  }
+
+  // méthode pour modifier une plateforme
+  public function updatePlateforme(Plateforme $plateforme){ // avec en paramètre un objet plateforme
+    $update_plateforme = $this -> pdo -> prepare( 'UPDATE plateforme SET nom_plateforme = :nom_plateforme WHERE id_plateforme = '.$plateforme->getIdPlateforme() );
+    $update_plateforme -> bindValue( ':nom_plateforme', $plateforme->getNomPlateforme(), PDO::PARAM_INT ); // j'affecte la valeur du formulaire au champ de la table
+    $update_plateforme -> execute();
+    $update_plateforme -> closeCursor();
+    return ($update_plateforme-> rowCount() ); // retourne le nbre de lignes
+  }
+
+  // méthode pour supprimer une plateforme par son id
+  public function deletePlateforme(Plateforme $plateforme){
+    $delete_plateforme = $this -> pdo -> prepare( 'DELETE FROM plateforme WHERE id_plateforme = '.$plateforme->getIdPlateforme() );
+    $delete_plateforme -> bindValue(':id_plateforme', $plateforme -> getIdPlateforme(), PDO:: PARAM_INT);
+    $delete_plateforme -> execute();
+    $delete_plateforme -> closeCursor();
+    return ($delete_plateforme-> rowCount() ); // retourne le nbre de lignes
+  }
+
+  // méthode pour afficher une liste de plateforme
+  public function getListPlateforme(){
+    $list_plateforme = $this->pdo -> query('SELECT * FROM plateforme;');
+    return $list_plateforme -> fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
