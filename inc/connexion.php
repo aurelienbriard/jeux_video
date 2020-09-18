f1576aa52ae1135402db37aea599c4100cb753ae
johann <johann.espanet@gmail.com>
2020-09-18 09:19:32 +0200

    ajout nav+ tableau index.php

--- inc/connexion.php
+++ inc/connexion.php
@@ -1,7 +1,5 @@
 <?php
 
-<?php
-
 try{ $bdd = new PDO('mysql:host=localhost;dbname=jeux_video;charset=utf8','root','');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //évite de renvoyer que des strings en tableu associatif
